54be58d9d758175ccc8d84b76f42790cbb277c33
CokdePutra <cokgdeputrawidnyana25@gmail.com>
2023-03-24 13:50:22 +0800

    Updating update siswa

--- update/proses_update_siswa.php
+++ update/proses_update_siswa.php
@@ -9,10 +9,16 @@ $angkatan = $_POST['angkatan'];
 $alamat = $_POST['alamat'];
 $no_telp = $_POST['telp_ortu'];
 
-$hasil = mysqli_query($koneksi, "UPDATE tb_siswa SET nis='$nis', nisn='$nisn', password='$password', nama_siswa='$nama_siswa', id_kelas='$id_kelas', angkatan='$angkatan', alamat='$alamat', telp_ortu='$no_telp' WHERE nis='$nis'");
+$cek_nisn = mysqli_num_rows(mysqli_query($koneksi, "SELECT nisn FROM tb_siswa WHERE nisn='$nisn'"));
 
-if (!$hasil) {
-    echo "<script>alert('Data Siswa gagal di Ubah'); window.location.href='../view-tabel/tb_siswa.php?page=siswa'</script>";
+if ($cek_nisn < 1) {
+    $hasil = mysqli_query($koneksi, "UPDATE tb_siswa SET nis='$nis', nisn='$nisn', password='$password', nama_siswa='$nama_siswa', id_kelas='$id_kelas', angkatan='$angkatan', alamat='$alamat', telp_ortu='$no_telp' WHERE nis='$nis'");
+
+    if (!$hasil) {
+        echo "<script>alert('Data Siswa gagal di Ubah'); window.location.href='../view-tabel/tb_siswa.php?page=siswa'</script>";
+    } else {
+        echo "<script>alert('Data Siswa berhasil di Ubah'); window.location.href='../view-tabel/tb_siswa.php?page=siswa'</script>";
+    }
 } else {
-    echo "<script>alert('Data Siswa berhasil di Ubah'); window.location.href='../view-tabel/tb_siswa.php?page=siswa'</script>";
+    echo "<script>alert('Maaf NISN Sudah Terpakai');window.location.href='../view-tabel/tb_siswa.php?page=siswa';</script>";
 }
